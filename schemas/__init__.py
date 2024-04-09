015cc2b7da3c36920ba60eedb1bfcbc356283819
MRAVITEJA390 <mraviteja390@gmail.com>
2024-04-10 00:32:08 +0530

    #added address pydantic validation schemas for response handling

---
+++ schemas/__init__.py
@@ -0,0 +1,5 @@
+from .address_book import AddressBookCreate, AddressBookUpdate, AddressBookDetail, AddressCoordinates, \
+    AddressWithDistance
+
+__all__ = ['AddressBookCreate', 'AddressBookUpdate', 'AddressBookDetail',
+           'AddressCoordinates', 'AddressWithDistance']
---
+++ schemas/address_book.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+
+
+class AddressBookBase(BaseModel):
+    name: str = Field(min_length=1)
+    latitude: float = Field(ge=-90, le=90, error_message='should be between 90 and -90')
+    longitude: float = Field(ge=-180, le=180)
+
+
+class AddressBookCreate(AddressBookBase):
+    pass
+
+
+class AddressBookUpdate(AddressBookBase):
+    pass
+
+
+class AddressBookDetail(AddressBookBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class AddressCoordinates(BaseModel):
+    latitude: float = Field(ge=-90, le=90, error_message='should be between 90 and -90')
+    longitude: float = Field(ge=-180, le=180)
+
+
+class AddressWithDistance(AddressBookBase):
+    id: int
+    distance: int
